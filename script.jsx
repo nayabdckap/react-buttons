a1edb5be6390019839ad5a5a23b6f48af028af83
nayabdckap <nayabsamar@dckap.com>
2019-03-18 18:59:52 +0530

    added component for Display - not working with all features

--- script.jsx
+++ script.jsx
@@ -1,30 +1,34 @@
-import React, {Component} from 'react';
-
-class MainComp extends Component {
+class MainComp extends React.Component {
     constructor(props) {
         super(props);
         this.incrementValue = this.incrementValue.bind(this)
         this.state = {
-            value: []
+            value: 0
         }
     }
 
     incrementValue(value) {
 
+        this.setState(
+            {
+                value: this.state.value + value
+            }
+        )
     }
 
     render() {
         return (
             <div>
-                <ButtonComp param="5" methodOne={this.incrementValue}/>
-                <ButtonComp param="10" methodOne={this.incrementValue}/>
+                <ButtonComp param="5" methodOne={this.incrementValue}/><br/>
+                <ButtonComp param="10" methodOne={this.incrementValue}/><br/>
                 <ButtonComp param="20" methodOne={this.incrementValue}/>
+                <DisplayComp display_param={this.state.value}/>
             </div>
         )
     }
 }
 
-class ButtonComp extends Component {
+class ButtonComp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -36,30 +40,30 @@ class ButtonComp extends Component {
         return (
             <div>
                 <input type="button"
-                       value="Multiples of "
-                       onClick={() => this.props.methodOne(this.props.param)}
+                       value={"Multiples of " + this.props.param}
+                       onClick={() => this.props.methodOne(parseInt(this.props.param))}
                 />
             </div>
         )
     }
 }
 
-// class LogicComp extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             value: []
-//         }
-//     }
-//
-//     render() {
-//         return (
-//             <div>
-//
-//             </div>
-//         )
-//     }
-// }
+class DisplayComp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Value: {this.props.display_param}</p>
+            </div>
+        )
+    }
+}
 
 
 ReactDOM.render(
